a7b44a88072f426f41b0dba10f52e1b0c2581ce3
blitzblue12o <a.camposagrado@gmail.com>
2016-06-12 12:57:02 -0700

    added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var del = require('del');
 
 gulp.task('hello', function() {
   console.log('Hello Alfred');
@@ -9,8 +11,27 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) //using gulp-sass
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
 });
